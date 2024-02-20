23c5aa92c89565d51c424edc9b35fa27bbafdee5
ecohen1125 <ecohen1125@gmail.com>
2024-02-20 15:52:15 -0500

    Working on project 3

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        Debug.Log("entered square");
+    }
+
+    void OnMouseExit()
+    {
+        // Pass 'null' to the texture parameter to use the default system cursor.
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
